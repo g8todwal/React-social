c5e005fd4aaad8057f52ca155446ad7abca5f366
g8todwal <g8todwal@gmail.com>
2020-08-05 23:54:50 +0530

    draft

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import Facebook from './Components/Facebook';
+
+function App() {
+  return (
+    <div className="App-header">
+      <h1>Facebook Auth Example</h1>
+      <Facebook />
+    </div>
+  );
+}
+
+export default App;
